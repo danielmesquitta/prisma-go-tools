c88c9169e5cccfabdd267cf26f3d74e9f7a6089c
Daniel Mesquita <danielmesquitta123@gmail.com>
2025-02-06 12:49:10 -0300

    refactor: no conflicts

--- cmd/root.go
+++ cmd/root.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"os"
-	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -17,8 +16,6 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	time.Local = time.UTC
-
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
